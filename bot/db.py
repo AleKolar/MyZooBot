10151f18013ce620d767d71e150065edb2d8ebca
AleKolar <gefest-173@rambler.ru>
2024-05-24 22:09:05 +0300

    and then4

--- bot/db.py
+++ bot/db.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from handlers.try_choosing_handlers import amount
 
-amount = 14
+amount = 13
 class Database:
     def __init__(self, db_file):
         self.connection = sqlite3.connect(db_file)
@@ -22,37 +22,27 @@ class Database:
             self.cursor.execute("INSERT INTO 'users' ('user_id') VALUES (?)", (user_id,))
             return self.connection.commit()
 
-    def add_pic(self):
+    def add_pic(self, user_id):
         self.cursor.execute('CREATE TABLE IF NOT EXISTS photos(amount_photo INTEGER PRIMARY KEY, photo BLOB)')
         i = self.amount
         #for i in range(13, 22):
         with open(f'{i}.jpeg', 'rb') as photo:
             h = photo.read()
             #self.cursor.execute('INSERT INTO photos(photo) VALUES(?)', [h])
-            self.cursor.execute("INSERT INTO photos (photo, amount_photo) VALUES (?, ?)", (h, i))
+            self.cursor.execute("INSERT INTO photos (photo, amount_photo, user_id) VALUES (?, ?, ?)", (h, i, user_id))
         return self.connection.commit()
+
+
+    def get_photo(self, user_id):
+        self.cursor.execute("SELECT photo FROM photos WHERE user_id=?", (user_id,))
+        photo = self.cursor.fetchone()
+        return photo[0] if photo else None
 
     def close(self):
         self.connection.close()
 
 
 a = Database('DB1')
-a.add_pic()
+#a.add_pic(121)
+print(a.get_photo(121))
 
-'''
-
-# Открываем соединение с базой данных
-conn = sqlite3.connect('mydatabase.db')
-cursor = conn.cursor()
-
-# Фотография и идентификационный номер
-photo_data = b'your_photo_data'
-photo_id = 'photo1'
-
-# Выполняем запрос SQL для добавления фотографии в таблицу photos
-cursor.execute("INSERT INTO photos (photo, amount_photo) VALUES (?, ?)", (photo_data, photo_id))
-
-# Сохраняем изменения и закрываем соединение с базой данных
-conn.commit()
-conn.close()
-'''
